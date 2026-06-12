720e6810d08328a50145a49f2d841b581894dc0a
emilnordstroem <emilstoeve@outlook.com>
2026-06-12 20:59:27 +0200

    Dailsy criteria implementation

--- src/scripts/rubiks-cube-algorithms/rubiks-cube-daisy-algorithm.ts
+++ src/scripts/rubiks-cube-algorithms/rubiks-cube-daisy-algorithm.ts
@@ -1,7 +1,49 @@
 import type { RubiksCube } from "../rubiks-cube-model";
 
 export function daisyAlgorithm (rubikscube: RubiksCube): RubiksCube {
-    if(){}
+    if(isDaisyComplete(rubikscube.Down)){
+        return rubikscube
+    }
+    
 }
 
-function isDaisyComplete(face: string[][])
+const center: string = 'yellow'
+const leaf: string = 'white' 
+
+function isDaisyComplete (face: string[][]): boolean {
+    for (let row = 0; row < face.length; row++) {
+        for (let column = 0; column < face[row].length; column++) {
+            const squareColor: string = face[row][column]
+            if (isCorner(row, column) && !hasCorrectColor(squareColor, leaf)) {
+                return false
+            } else if (isLeaf(row, column) && !hasCorrectColor(squareColor, leaf)) {
+                return false;
+            } else if (isCenter(row, column) && !hasCorrectColor(squareColor, center)) {
+                return false
+            } 
+        }
+    }
+    return true
+}
+
+function hasCorrectColor(colorOfSquare: string, colorToMatch: string){
+    return colorOfSquare == colorToMatch
+}
+
+function isCorner (row: number, column: number): Boolean {
+    return row == 0 && column == 0
+        || row == 0 && column == 2
+        || row == 2 && column == 0
+        || row == 2 && column == 2
+}
+
+function isLeaf (row: number, column: number): boolean {
+    return row == 0 && column == 1
+        || row == 1 && column == 0
+        || row == 1 && column == 2
+        || row == 2 && column == 1
+}
+
+function isCenter (row: number, column: number): Boolean {
+    return row == 1 && column == 1
+}
