3e38507e7bc64b078e2eb2e3888ba2cd134efa09
emilnordstroem <emilstoeve@outlook.com>
2026-06-12 19:41:53 +0200

    Up/Down Left/Right rotation

--- src/scripts/rubiks-cube-rotation.ts
+++ src/scripts/rubiks-cube-rotation.ts
@@ -18,16 +18,34 @@ function rotateFaceCounterClockwise(face: string[][]): string[][] {
 }
 
 // rotation algorithms
-/*
 function upFaceRotationLeft (rubikscube: RubiksCube, movements: number): RubiksCube {
-    
+    for (let movement = 1; movement <= movements; movement++) {
+        rubikscube.Up = rotateFaceClockwise(rubikscube.Up)
+        const tempRow = [...rubikscube.Front[0]]
 
+        rubikscube.Front[0] = [...rubikscube.Right[0]]
+        rubikscube.Right[0] = [...rubikscube.Back[0]]
+        rubikscube.Back[0]  = [...rubikscube.Left[0]]
+        rubikscube.Left[0]  = tempRow
+    }
+    return rubikscube
 }
+
 
 function upFaceRotationRight (rubikscube: RubiksCube, movements: number): RubiksCube {
+    for (let movement = 1; movement <= movements; movement++) {
+        rubikscube.Up = rotateFaceClockwise(rubikscube.Up)
+        const tempRow = [...rubikscube.Front[0]]
 
+        rubikscube.Front[0] = [...rubikscube.Left[0]]
+        rubikscube.Right[0] = [...rubikscube.Front[0]]
+        rubikscube.Back[0]  = [...rubikscube.Right[0]]
+        rubikscube.Right[0]  = tempRow
+    }
+    return rubikscube
 }
 
+/*
 function frontFaceRotationLeft (rubikscube: RubiksCube, movements: number): RubiksCube {
 
 }
@@ -35,15 +53,35 @@ function frontFaceRotationLeft (rubikscube: RubiksCube, movements: number): Rubi
 function frontFaceRotationRight (rubikscube: RubiksCube, movements: number): RubiksCube {
 
 }
+*/
 
 function downFaceRotationLeft (rubikscube: RubiksCube, movements: number): RubiksCube {
+    for (let movement = 1; movement <= movements; movement++) {
+        rubikscube.Down = rotateFaceClockwise(rubikscube.Down)
+        const tempRow = [...rubikscube.Front[2]]
 
+        rubikscube.Front[2] = [...rubikscube.Right[2]]
+        rubikscube.Right[2] = [...rubikscube.Back[2]]
+        rubikscube.Back[2]  = [...rubikscube.Left[2]]
+        rubikscube.Left[2]  = tempRow
+    }
+    return rubikscube
 }
 
 function downFaceRotationRight (rubikscube: RubiksCube, movements: number): RubiksCube {
+    for (let movement = 1; movement <= movements; movement++) {
+        rubikscube.Up = rotateFaceClockwise(rubikscube.Up)
+        const tempRow = [...rubikscube.Front[2]]
 
+        rubikscube.Front[2] = [...rubikscube.Left[2]]
+        rubikscube.Right[2] = [...rubikscube.Front[2]]
+        rubikscube.Back[2]  = [...rubikscube.Right[2]]
+        rubikscube.Right[2]  = tempRow
+    }
+    return rubikscube
 }
 
+/*
 function backFaceRotationLeft (rubikscube: RubiksCube, movements: number): RubiksCube {
 
 }
