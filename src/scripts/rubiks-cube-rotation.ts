f7168d90bb90efa0602d042e976efb9fec6e9796
emilnordstroem <emilstoeve@outlook.com>
2026-06-12 17:40:12 +0200

    Skeleton for rotation

---
+++ src/scripts/rubiks-cube-rotation.ts
@@ -0,0 +1,51 @@
+import type { RubiksCube } from "./rubiks-cube-model";
+
+/*
+function UpFaceRotationLeft (rubikscube: RubiksCube, movements: number): RubiksCube {
+    
+}
+
+function UpFaceRotationRight (rubikscube: RubiksCube, movements: number): RubiksCube {
+
+}
+
+function FrontFaceRotationLeft (rubikscube: RubiksCube, movements: number): RubiksCube {
+
+}
+
+function FrontFaceRotationRight (rubikscube: RubiksCube, movements: number): RubiksCube {
+
+}
+
+function DownFaceRotationLeft (rubikscube: RubiksCube, movements: number): RubiksCube {
+
+}
+
+function DownFaceRotationRight (rubikscube: RubiksCube, movements: number): RubiksCube {
+
+}
+
+function BackFaceRotationLeft (rubikscube: RubiksCube, movements: number): RubiksCube {
+
+}
+
+function BackFaceRotationRight (rubikscube: RubiksCube, movements: number): RubiksCube {
+
+}
+
+function LeftFaceRotationUp (rubikscube: RubiksCube, movements: number): RubiksCube {
+
+}
+
+function LeftFaceRotationDown (rubikscube: RubiksCube, movements: number): RubiksCube {
+
+}
+
+function RightFaceRotationUp (rubikscube: RubiksCube, movements: number): RubiksCube {
+
+}
+
+function RightFaceRotationDown (rubikscube: RubiksCube, movements: number): RubiksCube {
+
+}
+*/
