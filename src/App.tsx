89dd3ee951757aaf36d9cbda66d0bc66b2f3b5c9
emilnordstroem <emilstoeve@outlook.com>
2026-06-14 10:08:25 +0200

    Formed rubikscube to a use state

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
+import { useState } from 'react'
 import Header from './components/Header'
 import { RubiksCube as RubiksCubeModel } from './scripts/rubiks-cube-model'
 import RubiksCube from './components/RubiksCube/RubiksCube'
 import './styles/App.css'
 
-let rubiksCube = new RubiksCubeModel()
-
 function App() {
-    return (
-      <>
-        <Header />
-        <RubiksCube rubiksCube={rubiksCube} />
-      </>
-    )
+  const [rubiksCube, setRubiksCube] = useState(() => new RubiksCubeModel())
+
+  return (
+    <>
+      <Header />
+      <RubiksCube rubiksCube={rubiksCube} />
+    </>
+  )
 }
 
 export default App
